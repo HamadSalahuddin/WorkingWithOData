312b900bec31be6c8ca17da8154d45122f0692c0
Hamad Salahuddin <hamadsalahuddin@gmail.com>
2020-11-03 15:56:00 +0500

    Added Odata Config and Controller endpoints (CRUD)

--- ProductService/ProductService/App_Start/WebApiConfig.cs
+++ ProductService/ProductService/App_Start/WebApiConfig.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ProductService.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
 using System.Web.Http;
 
 namespace ProductService
@@ -10,15 +10,21 @@ namespace ProductService
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel()
             );
+            // Web API routes
+            //    config.MapHttpAttributeRoutes();
+
+            //    config.Routes.MapHttpRoute(
+            //        name: "DefaultApi",
+            //        routeTemplate: "api/{controller}/{id}",
+            //        defaults: new { id = RouteParameter.Optional }
+            //    );
         }
     }
 }
